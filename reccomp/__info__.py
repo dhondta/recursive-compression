379ee06badf9693abaa8e8910423d0df53e112ab
dhondta <alexandre.dhondt@gmail.com>
2019-08-14 12:28:09 +0200

    New release

---
+++ reccomp/__info__.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+"""Recursive compression package information.
+
+"""
+__all__ = ["__author__", "__copyright__", "__license__", "__version__"]
+__author__ = "Alexandre D'Hondt"
+__version__ = "2.8"
+__copyright__ = "A. D'Hondt"
+__license__   = "agpl-3.0"
