3141ca3cfceb0d4c559cee86ce2e9850597085d7
dhondta <alexandre.dhondt@gmail.com>
2020-01-18 10:02:35 +0100

    Fixed bug in ts.code_replace

--- reccomp/__init__.py
+++ reccomp/__init__.py
@@ -1,7 +1,7 @@
 """ Recursive (de)compression module.
 """
 import patoolib
-from tinyscript import ts
+from tinyscript import code
 
 from .compressor import Compressor
 from .decompressor import Decompressor
@@ -11,18 +11,18 @@ __all__ = ["Compressor", "Decompressor"]
 
 
 # patch patoolib.util.backtick at runtime to silent annoying subprocess messages
-ts.code_replace(patoolib.util.backtick,
-                "stdout=subprocess.PIPE)",
-                "stdout=subprocess.PIPE, stderr=subprocess.PIPE)")
+code.replace(patoolib.util.backtick,
+             "stdout=subprocess.PIPE)",
+             "stdout=subprocess.PIPE, stderr=subprocess.PIPE)")
 # patch patoolib.util.log_info at runtime to silent annoying log messages
-ts.code_replace(patoolib.util.log_info,
-                "print(\"patool:\", msg, file=out)",
-                "pass")
+code.replace(patoolib.util.log_info,
+             "print(\"patool:\", msg, file=out)",
+             "pass")
 # patch patoolib.util.run at runtime to silent annoying subprocess.call messages
-ts.code_replace(patoolib.util.run,
-                "kwargs['stdout'] = devnull",
+code.replace(patoolib.util.run,
+             "kwargs['stdout'] = devnull",
            "kwargs['stdout'] = devnull\n            kwargs['stderr'] = devnull")
 # patch patoolib.util.guess_mime_file to force mime DB guess for some formats
-ts.code_replace(patoolib.util.guess_mime_file,
-                "if ext.lower() in ('.alz',):",
-                "if ext.lower() in ('.alz','.bz2','.gz','.xz'):")
+code.replace(patoolib.util.guess_mime_file,
+             "if ext.lower() in ('.alz',):",
+             "if ext.lower() in ('.alz','.bz2','.gz','.xz'):")
